6bd057057522e8f788ad947980615be9d8914278
Kien-Tran <ttkien2004@gmail.com>
2024-10-29 09:58:53 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
+// const socket = io("http://localhost:5000");
+
+function App() {
+  // useEffect(() => {
+  //   const socket = io.connect("http://localhost:5000");
+  // }, []);
+  return <div>hello world</div>;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,3 @@
+// const io = require("socket.io-client");
+import { io } from "socket.io-client";
+const socket = io.connect("http://localhost:5000");
